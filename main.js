79b3a0e8b3b59e65c0e4860540e9ce862e6077e1
DevMattDavies <mdavies.me@pm.me>
2022-05-07 19:42:50 +0100

    Added all letter buttons and finished layout

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-// - Store word list in data.js file
-//      - Create function to iterate over array and return only 5 letter words
-//      - Create function to pick a random word from 5 letter list & store in variable
+// - Create .js files to store word answers and word candidates
+//      - Create function to iterate over array and return a random word
 // - Create new game
 //      - Event listener on button
 //      - Clear all previous entries on screen
 //      - Take random 5 letter word and store in new array
 //      - Separate word into individual letters in array
+// - Create 'letter' buttons for user letter input
+//      - Add event listeners to each button, e.target.value or innerText?
 // - Check user input against correct word
 //      - Event listener on 'Submit word' button to run function
 //      - Convert word to array of individual letters
@@ -22,8 +23,9 @@ const answerWordsLength = wordAnswers.words.length;
 const candidateWordsLength = wordCandidates.words.length;
 
 
-// Function to return a random word from answerWordsLength
+// Function to return a random word from answerWordsLength & clear previous entries
 function getWord() {
+    // Add clear all word/letter entry code here
     let randomNum = Math.random();
     let intNum = Math.floor(randomNum * answerWordsLength)
     let selectedWord = wordAnswers.words[intNum];
@@ -37,3 +39,25 @@ let gameWord = (getWord());
 // Add event listener on new game button to get new word
 let newGameButton = document.querySelector('#new-game-button');
 newGameButton.addEventListener("click", getWord);
+
+// Create letter input buttons
+
+const letters = [
+    "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", '⌫', "C", "V", "B", "N", "M", "Enter"
+]
+
+
+
+for (i = 0; i < letters.length; i++) {
+    // Create new button
+    let newLetterButton = document.createElement('button');
+    // Set class name unique to each letter
+    let letterPosition = `letter-${letters[i]}`;
+    newLetterButton.setAttribute('id', letterPosition);
+    newLetterButton.setAttribute('class', 'letter-button');
+    // Set innerText to unique letter
+    newLetterButton.innerText = letters[i];
+    // Append button to 'input-buttons' section
+    let targetSection = document.querySelector('#input-buttons');
+    targetSection.appendChild(newLetterButton);
+}
