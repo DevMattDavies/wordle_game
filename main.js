02b5ef53f4bb69e007261e0382f75d7ea43194af
DevMattDavies <mdavies.me@pm.me>
2022-05-07 16:04:31 +0100

    Working function which returns a list of all 5 letter words

--- main.js
+++ main.js
@@ -17,3 +17,48 @@
 //      - User letters turn green if correct in right position, yellow if correct but wrong position, grey if wrong
 //      - Confirm box if right answer inputted
 //          - If statement to wipe board if play again selected
+
+
+// Word stored in variable
+let words = wordsList.words;
+// let wordsToArray = words[0].split();
+// console.log(wordsToArray);
+
+function onlyFiveLetterWords() {
+    let wordArray = [];
+    for (i = 0; i < words.length; i++) {
+        if (words[i].length === 5) {
+            let selectedWord = words[i];
+            selectedWord = selectedWord.split();
+            wordArray.push(selectedWord);
+        }
+    }
+    return wordArray;
+}
+
+let wordSelection = onlyFiveLetterWords();
+console.log(wordSelection);
+
+function getWord() {
+    let fiveLetterWords = [];
+    for (i = 0; i < words.length; i++) {
+        if (words[i].length === 5) {
+            let correctWord = words[i];
+            fiveLetterWords = fiveLetterWords.push(correctWord);
+        }
+    }
+    let randomNum = Math.random();
+    let intNum = Math.floor(randomNum * wordsLength)
+    let selectedWord = fiveLetterWords[intNum];
+    console.log(selectedWord);
+}
+getWord();
+
+// let wordsLength = words.length
+// let randomNum = Math.random();
+// let intNum = Math.floor(randomNum * wordsLength)
+// let word = wordsList.words[intNum];
+// console.log(word);
+
+
+// 
