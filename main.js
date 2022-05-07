ec21407cf91641eecd74807fba4d94416fd8c726
DevMattDavies <mdavies.me@pm.me>
2022-05-07 13:31:02 +0100

    Plan finished, grid layout complete

---
+++ main.js
@@ -0,0 +1,19 @@
+// - Get dictionary API
+//      - Fetch API & convert to .json
+//      - .then to store as variable
+// - Function to return only 5 letter words
+//      - Store all words in array
+// - Create new game
+//      - Event listener on button
+//      - Clear all previous entries on screen
+//      - Take random 5 letter word and store in new array
+//      - Separate word into individual letters in array
+// - Check user input against correct word
+//      - Event listener on 'Submit word' button to run function
+//      - Convert word to array of individual letters
+//      - Add letters one by one into the line
+//          - Use setTimeout to delay between them appearing
+//      - If statements to check letter inputs against correct letters array
+//      - User letters turn green if correct in right position, yellow if correct but wrong position, grey if wrong
+//      - Confirm box if right answer inputted
+//          - If statement to wipe board if play again selected
